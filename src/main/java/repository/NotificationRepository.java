f19b1a34462cb36fe3fefd58100f87d4c3aca931
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-02 12:50:51 +0530

    Updated NotificationRepository

--- src/main/java/repository/NotificationRepository.java
+++ src/main/java/repository/NotificationRepository.java
@@ -1,4 +1,14 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface NotificationRepository {
+import com.rev.app.revhire.entity.Notification;
+import com.rev.app.revhire.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+
+    List<Notification> findByUserAndIsReadOrderByCreatedAtDesc(User user, boolean isRead);
+
+    List<Notification> findByUserAndIsReadFalseOrderByCreatedAtDesc(User user);
 }
