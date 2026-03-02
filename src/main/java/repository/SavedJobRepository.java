e0f1bc7a90854eebeee7b54656e40006ef83e4cc
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-02 12:51:02 +0530

    Updated SavedJobRepository

--- src/main/java/repository/SavedJobRepository.java
+++ src/main/java/repository/SavedJobRepository.java
@@ -1,4 +1,18 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface SavedJobRepository {
+import com.rev.app.revhire.entity.Job;
+import com.rev.app.revhire.entity.JobSeeker;
+import com.rev.app.revhire.entity.SavedJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface SavedJobRepository extends JpaRepository<SavedJob, Long> {
+    List<SavedJob> findByJobSeeker(JobSeeker jobSeeker);
+
+    Optional<SavedJob> findByJobSeekerAndJob(JobSeeker jobSeeker, Job job);
+
+    void deleteByJobSeekerAndJob(JobSeeker jobSeeker, Job job);
+
+    boolean existsByJobSeekerAndJob(JobSeeker jobSeeker, Job job);
 }
