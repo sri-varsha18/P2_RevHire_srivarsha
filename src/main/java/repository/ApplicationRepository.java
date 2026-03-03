abf5d097fb3f91408e7baa93d4fd7d1bab9b042a
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:10 +0530

    Updated ApplicationRepository

--- src/main/java/repository/ApplicationRepository.java
+++ src/main/java/repository/ApplicationRepository.java
@@ -1,4 +1,22 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface ApplicationRepository {
+import com.rev.app.revhire.entity.Application;
+import com.rev.app.revhire.entity.Job;
+import com.rev.app.revhire.entity.JobSeeker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import java.util.List;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long>, JpaSpecificationExecutor<Application> {
+    List<Application> findByJob(Job job);
+
+    List<Application> findByJobSeeker(JobSeeker jobSeeker);
+
+    boolean existsByJobAndJobSeeker(Job job, JobSeeker jobSeeker);
+
+    long countByJob(Job job);
+
+    long countByJob_Company_CompanyId(Long companyId);
+
+    long countByJob_Company_CompanyIdAndStatus(Long companyId, String status);
 }
