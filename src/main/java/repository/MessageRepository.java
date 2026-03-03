5f1c32dba675fd1db8ce7a0d38a16fa71924d511
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:11 +0530

    Updated MessageRepository

--- src/main/java/repository/MessageRepository.java
+++ src/main/java/repository/MessageRepository.java
@@ -1,4 +1,17 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface MessageRepository {
+import com.rev.app.revhire.entity.Message;
+import com.rev.app.revhire.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByReceiverOrderBySentAtDesc(User receiver);
+
+    List<Message> findBySenderOrderBySentAtDesc(User sender);
+
+    @Query("SELECT m FROM Message m WHERE (m.sender = :user1 AND m.receiver = :user2) OR (m.sender = :user2 AND m.receiver = :user1) ORDER BY m.sentAt ASC")
+    List<Message> findMessagesBetween(@Param("user1") User user1, @Param("user2") User user2);
 }
