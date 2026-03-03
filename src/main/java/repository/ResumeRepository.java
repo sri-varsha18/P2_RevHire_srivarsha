46c5e719ea16e6d1c29f8ccf8b61c0c116877151
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:13 +0530

    Updated ResumeRepository

--- src/main/java/repository/ResumeRepository.java
+++ src/main/java/repository/ResumeRepository.java
@@ -1,4 +1,10 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface ResumeRepository {
+import com.rev.app.revhire.entity.JobSeeker;
+import com.rev.app.revhire.entity.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+    Optional<Resume> findByJobSeeker(JobSeeker jobSeeker);
 }
