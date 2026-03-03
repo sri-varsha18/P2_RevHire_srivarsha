080f833769a5b27256d93fdcb05a4c54b3522448
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:20 +0530

    Updated CompanyRestController

--- src/main/java/rest/CompanyRestController.java
+++ src/main/java/rest/CompanyRestController.java
@@ -1,5 +1,30 @@
-package rest;
+package com.rev.app.revhire.rest;
 
-public class CompanyRestController
-{
+import com.rev.app.revhire.dto.CompanyDto;
+import com.rev.app.revhire.mapper.CompanyMapper;
+import com.rev.app.revhire.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/companies")
+public class CompanyRestController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public List<CompanyDto> getAllCompanies() {
+        return companyService.getAllCompanies().stream()
+                .map(CompanyMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompanyDto> getCompany(@PathVariable Long id) {
+        return ResponseEntity.ok(CompanyMapper.toDto(companyService.getCompanyById(id)));
+    }
 }
