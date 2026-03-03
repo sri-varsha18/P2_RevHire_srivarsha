523dd77f3f660d5c4ae097a33399aee6bb715ca9
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:01 +0530

    Updated CompanyMapper

--- src/main/java/mapper/CompanyMapper.java
+++ src/main/java/mapper/CompanyMapper.java
@@ -1,4 +1,34 @@
-package mapper;
+package com.rev.app.revhire.mapper;
+
+import com.rev.app.revhire.dto.CompanyDto;
+import com.rev.app.revhire.entity.Company;
 
 public class CompanyMapper {
+    public static CompanyDto toDto(Company company) {
+        if (company == null)
+            return null;
+        return CompanyDto.builder()
+                .companyId(company.getCompanyId())
+                .name(company.getName())
+                .industry(company.getIndustry())
+                .size(company.getSize())
+                .description(company.getDescription())
+                .website(company.getWebsite())
+                .location(company.getLocation())
+                .build();
+    }
+
+    public static Company toEntity(CompanyDto dto) {
+        if (dto == null)
+            return null;
+        return Company.builder()
+                .companyId(dto.getCompanyId())
+                .name(dto.getName())
+                .industry(dto.getIndustry())
+                .size(dto.getSize())
+                .description(dto.getDescription())
+                .website(dto.getWebsite())
+                .location(dto.getLocation())
+                .build();
+    }
 }
