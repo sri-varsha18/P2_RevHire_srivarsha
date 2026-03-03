8e679c2d36c56e41a68022f85326e1e6b9c95def
sri-varsha18 <srivarshapogiri@gmail.com>
2026-03-03 23:00:10 +0530

    Updated CompanyRepository

--- src/main/java/repository/CompanyRepository.java
+++ src/main/java/repository/CompanyRepository.java
@@ -1,4 +1,9 @@
-package repository;
+package com.rev.app.revhire.repository;
 
-public interface CompanyRepository {
+import com.rev.app.revhire.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByName(String name);
 }
